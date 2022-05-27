01a738879c9ffb6f693254f511db25582082cd3d
epic.chen <epicer.chen@gmail.com>
2022-05-28 01:43:49 +0800

    add router

--- src/components/Breadcrumbs.vue
+++ src/components/Breadcrumbs.vue
@@ -1,5 +1,5 @@
 <template>
-    <span v-for="(el, i) in nameDictionary != {} ? map(items) : items" :key="i">
+    <span v-for="(el, i) in nameDictionary != {} ? map(node) : node" :key="i">
         <span v-if="i > 0" v-html="symbol" />
         <button @click="goTo(i)">{{ el }}</button>
     </span>
@@ -7,19 +7,24 @@
 
 <script lang="ts">
 import { defineComponent, PropType, toRefs } from "vue";
+import { RouteLocationNormalizedLoaded, Router } from "vue-router";
 
 export default defineComponent({
     name: "Breadcrumbs",
     props: {
-        'symbol': {
+        path: {
+            type: String,
+            unique: true,
+            default: ''
+        },
+        router: {
+            type: Object as PropType<Router>,
+            unique: true,
+        },
+         symbol: {
             type: String,
             unique: false,
             default: '>>'
-        },
-        path: {
-            type: String,//改成Router
-            unique: true,
-            default: ''
         },
         nameDictionary: {
             type: Object as PropType<{ [key: string]: string }>,
@@ -29,27 +34,18 @@ export default defineComponent({
     },
     setup(props) {
         const { path } = toRefs(props)
-        const items: string[] = path.value.split("/");
+        const node: string[] = path.value.split("/");
 
-        const map = (items: string[]) => {
-            let pItems: string[] = []
-            items.forEach(el => {
-                pItems.push(
-                    props.nameDictionary[el] != undefined
-                        ? props.nameDictionary[el] : el)
-            });
-            return pItems
+        const map = (items: string[]) =>
+            items.map((el) => props.nameDictionary[el] != undefined
+                ? props.nameDictionary[el] : el)
+
+        const goTo = (endIndex: number) => {
+            let path = node.slice(0, endIndex + 1).join("/")
+            props.router!.push(path == "" ? "/" : path)
         }
 
-        const goTo = (endPathIndex: number) => {
-            let goToParh = ""
-            for (let i = 0; i <= endPathIndex; i++) {
-                goToParh += `${items[i]}/`
-            }
-            console.log(goToParh)
-        }
-
-        return { items, goTo,   map }
+        return { node, goTo, map }
     }
 })
 </script>
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,37 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import Breadcrumbs from './components/Breadcrumbs.vue'
+import { createRouter, createWebHashHistory, createWebHistory, RouteRecordRaw } from 'vue-router'
 
-createApp(App).mount('#app')
+const routes: Array<RouteRecordRaw> = [
+    {
+        path: '/',
+        name: 'home',
+        component: App
+    },
+    {
+        path: '/DcpDefinition/DcpDefinition/2bda45f9-8077-4c44-bcea-a84610c9a84e',
+        name: '2bda45f9-8077-4c44-bcea-a84610c9a84e',
+        component: App
+    },
+    {
+        path: '/DcpDefinition/DcpDefinition',
+        name: 'DcpDefinitionDcpDefinition',
+        component: App
+    },
+    {
+        path: '/DcpDefinition',
+        name: 'DcpDefinition',
+        component: App
+    },
+]
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes
+})
+
+createApp(App)
+    .component("Breadcrumbs", Breadcrumbs)
+    .use(router)
+    .mount('#app')
