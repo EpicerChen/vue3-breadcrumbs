ac0705eb0dcc7c5e57f41c823efe70e77123604a
epic.chen <epicer.chen@gmail.com>
2022-12-14 21:42:34 +0800

    remove style

--- src/App.vue
+++ src/App.vue
@@ -15,14 +15,3 @@ const nameDictionary = {
   "c": "第三層"
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
